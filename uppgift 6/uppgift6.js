223ee3da3cc37ea3abe50fac261949d69b2b7876
Mohammed Darrh <mmdarrah@hotmail.com>
2018-11-02 11:12:38 +0100

    solving the big nummber problem

--- uppgift 6/uppgift6.js
+++ uppgift 6/uppgift6.js
@@ -7,38 +7,39 @@ OBS!!! numret måste sluta med "!" föratt programet beräkna fakultet
 
 
 
+let inPut = prompt("Ange ett heltal mellan 1 och 1000 föratt programet beräkna fakultet .",`Glöm inte "!" i sluten av numret`);
 
 
 // While loop för att kolla om användarens input är korrekt
 while (true) {
     
-    let inPut = prompt("Ange ett heltal mellan 1 och 1000 föratt programet beräkna fakultet .",`Glöm inte "!" i sluten av numret`);
-
-
+    
+    
     // Om användare klicka på avbryt programmet ska se Hejdå och avsluta
-        if (inPut === null){
+    if (inPut === null){
         alert("Hejdå")
         break;
     }
-
-
+    
+    
     //Programmet ska kolla om numret sluta med ! och mellan 1 till 1000 sedan programmet ska beräkna fakultet
-    if  ((inPut.endsWith("!")) && ((inPut >=1 && inPut <= 1000) )){
+    if  ( inPut.endsWith("!") ){
         inPut = inPut.substring (0,inPut.length -1);
+        if (inPut >=1 && inPut <= 1000){
         /*efter kolla på "!" i sluten av numret sedan substring method ska ta bort "!"
         och sedan skicka nummret till fakuletet function*/
         
         document.write("Fakultet av " +inPut+"!"+" är "+fakultet(inPut)+("."));
         break;
-              
+        }     
     }
+    // Om användare lägga till file input
+    else if (inPut != inPut.endsWith("!")||(isNaN(inPut))) {
+         document.write(`Fail input. OBS!! Du måste lägga till ett heltal mellan 1 och 1000 med "!" I slutet av numret för att beräkna fakulteten.`);
+         break;
+     }
 
     
-   // Om användare lägga till file input
-   else if (inPut != inPut.endsWith("!")|| (inPut %1 === 0)||(isNaN(inPut))) {
-        document.write(`Fail input. OBS!! Du måste lägga till ett heltal med "!" I slutet av numret för att beräkna fakulteten.`);
-        
-    }
    // loop slut
 
 
